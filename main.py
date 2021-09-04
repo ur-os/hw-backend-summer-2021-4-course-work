bf3968a4269dac94518bf2c240e8dcf6146a4cb3
urick0s <bluiote@gmail.com>
2021-09-05 01:50:20 +0300

    insert my token

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import os
 from app.web.app import setup_app
 from aiohttp.web import run_app
 
+FILE_PATH = ''
+
 if __name__ == "__main__":
     run_app(setup_app(config_path=os.path.join(os.path.abspath(__file__), 'config.yml')))
